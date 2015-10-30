0dd8c02d8151a03839cbff37a47ea616a24b3a45
Achraf Cherti <Asher256@users.noreply.github.com>
2015-10-30 17:38:57 -0400

    warning solved

--- misc/sizeof.c
+++ misc/sizeof.c
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
 	//affiche: void *      int     long
-	printf("%i %i %i",sizeof(void *), sizeof(int),sizeof(long));
+	printf("%i %i %i",(int)sizeof(void *), (int)sizeof(int),(int)sizeof(long));
 	return 0;
 }
 
